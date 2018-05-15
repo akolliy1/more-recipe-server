7d9a4abbaf29c924464887a5782aa0235afd25df
akolliy1 <adesanmiakoladedotun@gmail.com>
2018-05-15 12:05:36 +0100

    webpack features update

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+import webpack from 'webpack'
+import path from 'path'
+
+module.exports = {
+  entry: './client/index.jsx',
+  output: {
+    filename: 'bundle.js',
+    publicPath: '/',
+    path: path.resolve(__dirname, 'client/dist/')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|gif|jpe?g|svg)/i,
+        use: ['file-loader']
+      }
+    ]
+  }
+}
