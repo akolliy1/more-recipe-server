9b933008e8439970524101c13d7d2fcf8fe3fcbc
akolliy1 <adesanmiakoladedotun@gmail.com>
2018-05-15 12:06:12 +0100

    webpack features update

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
         use: ['style-loader', 'css-loader']
       },
       {
-        test: /\.(png|gif|jpe?g|svg)/i,
+        test: /\.(png|)/i,
         use: ['file-loader']
       }
     ]
