4a81d3b03179179c6b2dee5d21759e52b4799607
akolliy1 <adesanmiakoladedotun@gmail.com>
2018-05-16 21:34:01 +0100

    gitinore changes

---
+++ client/src/redux/actions/index.jsx
@@ -0,0 +1,5 @@
+import { ADD_ARTICLE } from '../constants/action-types'
+
+export const addArticle = article => (
+  { type: ADD_ARTICLE, payload: article }
+)
---
+++ client/src/redux/reducers/index.jsx
@@ -0,0 +1,15 @@
+import { ADD_ARTICLE } from '../constants/action-types'
+
+const initialState = {
+  articles: []
+}
+const rootReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_ARTICLE:
+      return { ...state, articles: state.articles.concat(action.payload) }
+      return state
+    default:
+      return state
+  }
+}
+export default rootReducer
