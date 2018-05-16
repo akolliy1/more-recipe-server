713557547ce968fd83d380b74b20345f98015b88
akolliy1 <adesanmiakoladedotun@gmail.com>
2018-05-16 21:35:02 +0100

    working on clients side

--- client/src/redux/reducers/index.jsx
+++ client/src/redux/reducers/index.jsx
@@ -4,12 +4,6 @@ const initialState = {
   articles: []
 }
 const rootReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case ADD_ARTICLE:
-      return { ...state, articles: state.articles.concat(action.payload) }
-      return state
-    default:
-      return state
-  }
+  
 }
 export default rootReducer
