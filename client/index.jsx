f3732902f1c0b59aeda61e7ef1c569495eef6a91
akolliy1 <adesanmiakoladedotun@gmail.com>
2018-05-20 16:58:30 +0100

    update on the first entry of the app

--- client/index.jsx
+++ client/index.jsx
@@ -1,17 +1,8 @@
-import 'babel-polyfill'
-import React from 'react'
-// import { render } from 'react-dom'
-import ReactDOM from "react-dom";
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
-import rootReducer from './redux/reducers'
 import App from './components/App'
 import index from './redux/index'
-const store = createStore(rootReducer)
-// render(<App />, document.getElementById('app'));
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('app')
-)
+
+console.log(`I'm a silly entry point`)
+
+const arr = [1, 2, 3]
+const iAmJavascriptES6 = () => console.log(...arr)
+window.iAmJavascriptES6 = iAmJavascriptES6
