a46181abc58db8476602e737120f4743a4222bb8
akolliy1 <adesanmiakoladedotun@gmail.com>
2018-05-20 18:59:21 +0100

    update on indexjs

--- client/redux/index.js
+++ client/redux/index.js
@@ -2,5 +2,9 @@ import store from './store/index'
 import { addArticle } from './actions/index'
 
 window.store = store
+store.subscribe(() => console.log('Look ma, Redux!!'))
 store.dispatch(addArticle({ name: 'React Redux Tutorial for Beginners', id: 1 }))
+store.subscribe(() => console.log('Look ma, Rex!!'))
+store.dispatch(addArticle({ name: 'React Redux for Beginners', id: 2 }))
+
 window.addArticle = addArticle
