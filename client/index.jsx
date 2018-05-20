b884b4ad82c6916cf1ddbed36283ff1c308fbb71
akolliy1 <adesanmiakoladedotun@gmail.com>
2018-05-20 10:57:52 +0100

    webpack entrypoint files

--- client/index.jsx
+++ client/index.jsx
@@ -1,17 +1,16 @@
 import 'babel-polyfill'
 import React from 'react'
 import { render } from 'react-dom'
-// import { Provider } from 'react-redux'
-// import { createStore } from 'redux'
-// import rootReducer from './src/reducers'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import rootReducer from './redux/reducers'
 import App from './components/App'
-import index from './src/index'
 
-// const store = createStore(rootReducer)
-render(<App />, document.getElementById('root'));
-// render(
-//   <Provider store={store}>
-//     <App />
-//   </Provider>,
-//   document.getElementById('app')
-// )
+const store = createStore(rootReducer)
+// render(<App />, document.getElementById('app'));
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('app')
+)
