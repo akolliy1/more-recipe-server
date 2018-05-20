08abadfcbdf75cd86b2c9f7db25bb5a81e5283b1
akolliy1 <adesanmiakoladedotun@gmail.com>
2018-05-20 10:11:54 +0100

    modification to webpackdev

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,24 +4,18 @@ const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 const common = require('./webpack.common.js')
-const Dotenv = require('dotenv-webpack')
+
 module.exports = merge(common, {
   entry: {
     app: './src/index.js',
     print: './client/index.jsx'
   },
-  devtool: 'cheap-eval-source-map',
+  devtool: 'inline-source-map',
   devServer: {
-    contentBase: './client/dist'
+    contentBase: './dist'
   },
   plugins: [
     new CleanWebpackPlugin(['dist']),
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoEmitOnErrorsPlugin(),
-    new Dotenv({
-      path: './.env',
-      safe: false
-    }),
     new HtmlWebpackPlugin({
       template: './client/index.html',
       filename: 'index.html',
