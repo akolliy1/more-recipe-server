642a0e12524a538413baa7ad7b6207eb8bdb5504
akolliy1 <adesanmiakoladedotun@gmail.com>
2018-05-20 10:13:01 +0100

    modification to webpackcommonjs

--- webpack.common.js
+++ webpack.common.js
@@ -1,8 +1,41 @@
 // import path from 'path'
 const path = require('path')
 // to allow Dotenv files
+const merge = require('webpack-merge')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const common = require('./webpack.common.js')
+const Dotenv = require('dotenv-webpack')
 
 module.exports = {
+  entry: {
+    app: './src/index.js',
+    print: './client/index.jsx'
+  },
+  devtool: 'cheap-eval-source-map',
+  devServer: {
+    contentBase: './client/dist'
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+    new Dotenv({
+      path: './.env',
+      safe: false
+    }),
+    new HtmlWebpackPlugin({
+      template: './client/index.html',
+      filename: 'index.html',
+      inject: 'body'
+    })
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
+  },
   watch: true,
   watchOptions: {
     poll: 1000,
@@ -26,13 +59,7 @@ module.exports = {
         test: /\.js?x$/,
         exclude: /(node_modules|bower_components)/,
         use: {
-          loader: 'babel-loader',
-          query: {
-            plugins: ['transform-class-properties', 'transform-object-rest-spread']
-          },
-          options: {
-            presets: ['react', 'env']
-          }
+          loader: 'babel-loader'
         },
         include: path.join(__dirname, '/client')
       },
