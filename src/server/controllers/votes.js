7382ee3e374095adba6566d9d20fd7a62aaf545c
akolliy1 <adesanmiakoladedotun@gmail.com>
2018-05-29 12:52:30 +0100

    new update

---
+++ src/server/controllers/votes.js
@@ -0,0 +1,32 @@
+import models from '../models'
+const {Recipe, Upvote, Downvote} = models
+
+export default class votes {
+  /**
+     * @description like - Recipe like
+     * @param {*} req
+     * @param {*} res
+     * @returns {Promise} promise
+     */
+  static like (req, res) {
+    const promise = new Promise((resolve, reject) => {
+      const { userId, recipeId } = req.body
+      const upvoted = async function () {
+        await Upvote.findOrCreate({userId, recipeId})
+          .spread((isLiked, liked) => {
+            if (liked) {
+              Recipe.findById(recipeId).then((recipe) => {
+                recipe.increment('upvotes')
+                  .then(() => {
+                    resolve(res.status(201).send({success: true, message: 'Recipe liked'}))
+                  })
+              })
+            }
+            reject(res.status(409).send({success: false, message: 'you\'ve liked'}))
+          })
+      }
+      upvoted()
+    })
+    return promise
+  }
+}
