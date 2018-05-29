4cd523981d42abac402302805f0e4a8b5036617b
akolliy1 <adesanmiakoladedotun@gmail.com>
2018-05-29 09:14:55 +0100

    new update

--- src/app.js
+++ src/app.js
@@ -4,38 +4,34 @@ import bodyParser from 'body-parser'
 import webpack from 'webpack'
 import path from 'path'
 import validator from 'express-validator'
-// import swaggerUi from 'swagger-ui-express';
+import dotenv from 'dotenv'
 import config from '../webpack.dev'
 import prodConfig from '../webpack.prod'
 import routes from './server/routes'
-// import apiDocs from './api-docs.json';
-
-require('babel-core/register')
-require('babel-polyfill')
-require('dotenv').config()
-
-const HMR = require('webpack-hot-middleware')
-
+import HMR from 'webpack-hot-middleware'
+import 'babel-core/register'
+import 'babel-polyfill'
+import webpackDevMiddleware from 'webpack-dev-middleware'
+dotenv.config()
 let compiler
-
 const app = express()
 
-if (process.env.NODE_ENV === 'production') {
-  compiler = webpack(prodConfig)
-} else {
-  compiler = webpack(config)
-  app.use(HMR(compiler))
+const checkEnvironment = function () {
+  if (process.env.NODE_ENV === 'production') {
+    compiler = webpack(prodConfig)
+  } else {
+    compiler = webpack(config)
+    app.use(HMR(compiler))
+  }
 }
 
-// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(apiDocs));;
-
-app.use(require('webpack-dev-middleware')(compiler, {
+checkEnvironment()
+app.use(webpackDevMiddleware(compiler, {
   noInfo: true,
   publicPath: config.output.publicPath
 }))
 
 app.use(logger('dev'))
-
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(validator())
@@ -51,5 +47,4 @@ app.all('*', (req, res) => {
     message: 'Route does not exist'
   })
 })
-
 export default app
