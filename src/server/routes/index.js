1be26265980793c31b91514f98b88627067b487c
akolliy1 <adesanmiakoladedotun@gmail.com>
2018-05-29 09:16:01 +0100

    new update

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,8 +1,8 @@
 import User from '../controllers/users'
+import favorites from "../controllers/favorites";
 import recipes from '../controllers/recipes'
 import reviews from '../controllers/reviews'
 import Validations from '../middlewares/userValidations'
-
 /**
  * @description Invoking Object Data as new object
  * @method  { any } method = new method
@@ -17,17 +17,18 @@ module.exports = (app) => {
   app.post('/api/users/signin', Validations.validateUserSignin, User.signIn)
   app.get('/api/users/:userId/profile', User.listAUser)
   // route to create recipes
-  app.post('/recipes', recipes.createRecipes)
-  app.get('/recipes', recipes.getAllRecipes)
+  app.route('/recipes')
+    .post(recipes.createRecipes)
+    .get(recipes.getAllRecipes)
   app.get('/recipes/:recipeId', recipes.getSingleRecipe)
   app.delete('/api/users/:userId', User.destroy)
 
   // review route
   app.post('/:recipeId/review', reviews.addReview)
 
-  // For any other request method on todo items, we're going to return "Method Not Allowed"
-  app.all('/api/users/:userId/items/comments', (req, res) =>
-    res.status(405).send({
-      message: 'Method Not Allowed'
-    }))
+  // favorites
+  app.route('/:recipeId/favorite')
+    .post(favorites.newFavorite)
+  app.delete('/:favoriteId', favorites.unfavorite)
+  app.get('/favorite', favorites.myFavorite)
 }
