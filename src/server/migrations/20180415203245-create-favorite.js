91c0f2085fbb5214dc89b229d452093c23715f1a
akolliy1 <adesanmiakoladedotun@gmail.com>
2018-05-30 17:13:30 +0100

    new update

--- src/server/migrations/20180415203245-create-favorite.js
+++ src/server/migrations/20180415203245-create-favorite.js
@@ -31,9 +31,9 @@ module.exports = {
         allowNull: false,
         type: Sequelize.DATE
       }
-    });
+    })
   },
-  down: (queryInterface, /* Sequelize*/) => {
-    return queryInterface.dropTable('Favorites');
+  down: (queryInterface /* Sequelize */) => {
+    return queryInterface.dropTable('Favorites')
   }
-};
+}
