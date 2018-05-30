00fba959ff41cfe9d854d56d25ab09bb77be9fa6
akolliy1 <adesanmiakoladedotun@gmail.com>
2018-05-30 17:14:40 +0100

    new update

--- src/server/migrations/20180415203434-create-upvote.js
+++ src/server/migrations/20180415203434-create-upvote.js
@@ -31,9 +31,9 @@ module.exports = {
         allowNull: false,
         type: Sequelize.DATE
       }
-    });
+    })
   },
-  down: (queryInterface, /* Sequelize*/) => {
-    return queryInterface.dropTable('Upvotes');
+  down: (queryInterface /* Sequelize */) => {
+    return queryInterface.dropTable('Upvotes')
   }
-};
+}
