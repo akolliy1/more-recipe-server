c021560e2da75b0695e030d483d1846c56d18717
akolliy1 <adesanmiakoladedotun@gmail.com>
2018-05-30 17:10:34 +0100

    new update

--- src/server/controllers/votes.js
+++ src/server/controllers/votes.js
@@ -3,42 +3,59 @@ const {Recipe, Upvote, Downvote} = models
 
 export default class votes {
   /**
-     * @description like - Recipe like
-     * @param {*} req
-     * @param {*} res
-     * @returns {Promise} promise
+     * @description Recipe unlike
+     * @param {*} req HTTP - Resquest
+     * @param {*} res HTTP - Response
+     * @example - check if it's Upvoted and it's recipe then downvote and
+     * incremeent recipe downvotes
+     * - check if it's neither Upvoted nor Downvoted then downvote and
+     * incremeent recipe downvotes
+     * - check if it isnt Upvoted but it's Downvoted then return confirmation for it's existence
+     * @returns {object} object
      */
-  static unlike (req, res) {
-    const { userId, recipeId } = req.body
-    const promise = new Promise((resolve, reject) => {
-
-    })
-    return promise
-    // const downvote =
+  static async unlike (req, res) {
+    const { userId, recipeId } = req.params
+    const isUpvoted = await Upvote.find({ where: { userId: userId, recipeId: recipeId } })
+    const isRecipe = await Recipe.find({where: {id: recipeId}})
+    const isDownvoted = await Downvote.find({ where: { userId: userId, recipeId: recipeId } })
+    if (!isRecipe) return res.status(404).send({success: false, message: 'recipe not found'})
+    if (isUpvoted && isRecipe && !isDownvoted) {
+      const unLikeRecipe = await isRecipe.decrement('upvotes')
+      const addDownVote = await Downvote.create({userId: userId, recipeId: recipeId})
+      if (unLikeRecipe && addDownVote) {
+        const increaseDownvoteRecipe = await isRecipe.increment('downvotes')
+        if (increaseDownvoteRecipe) return res.status(201).send({success: true, message: 'unliked'})
+      }
+    } if (!isUpvoted && isRecipe && !isDownvoted) {
+      const addDownVote = await Downvote.create({ userId: userId, recipeId: recipeId })
+      const increaseDownvoteRecipe = await isRecipe.increment('downvotes')
+      if (addDownVote && increaseDownvoteRecipe) return res.status(201).send({success: true, message: 'unliked'})
+    } if (isUpvoted && isRecipe && isDownvoted) {
+      const deleteDownvote = await isUpvoted.destroy()
+      if (deleteDownvote) return res.status(404).send({success: true, message: 'deleted'})
+    } else { return res.status(409).send({success: false, message: 'already downvoted'}) }
   }
   /**
-     * @description like - Recipe like
-     * @param {*} req
-     * @param {*} res
+     * @description  Recipe like
+     * @param {*} req HTTP - Resquest
+     * @param {*} res HTTP - Response
      * @returns {Promise} promise
      */
-  static like (req, res) {
-    const promise = new Promise((resolve, reject) => {
-      const { userId, recipeId } = req.body
-      const upvoted = async function () {
-        await Upvote.findOrCreate({userId, recipeId})
-          .spread((isLiked, liked) => {
-            if (liked) {
-              Recipe.findById(recipeId).then((recipe) => {
-                recipe.increment('upvotes')
-                resolve(res.status(201).send({success: true, message: 'Recipe liked'}))
-              })
-            }
-            reject(res.status(409).send({success: false, message: 'you\'ve liked'}))
-          })
+  static async like (req, res) {
+    const {recipeId, userId} = req.params
+    const isUpvoted = await Upvote.find({where: {userId: userId, recipeId: recipeId}})
+    if (isUpvoted) {
+      return res.status(409).send({success: false, message: 'already liked'})
+    }
+    const upvoted = await Upvote.create({recipeId: recipeId, userId: userId})
+    if (upvoted) {
+      const isRecipe = await Recipe.findOne({ where: { id: recipeId } })
+      if (isRecipe) {
+        const recipeLiked = await isRecipe.increment('upvotes')
+        if (recipeLiked) {
+          return res.status(201).send({ success: true, message: 'liked' })
+        }
       }
-      upvoted()
-    })
-    return promise
+    }
   }
 }
