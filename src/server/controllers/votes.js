b33694b3df050ca63987555e0728f765174c1ead
akolliy1 <adesanmiakoladedotun@gmail.com>
2018-06-01 18:40:21 +0100

    new update

--- src/server/controllers/votes.js
+++ src/server/controllers/votes.js
@@ -1,4 +1,5 @@
 import models from '../models'
+import Sequelize from 'sequelize'
 const {Recipe, Upvote, Downvote} = models
 
 export default class votes {
@@ -6,18 +7,19 @@ export default class votes {
      * @description Recipe unlike
      * @param {*} req HTTP - Resquest
      * @param {*} res HTTP - Response
-     * @example - check if it's Upvoted and it's recipe then downvote and
-     * incremeent recipe downvotes
-     * - check if it's neither Upvoted nor Downvoted then downvote and
-     * incremeent recipe downvotes
-     * - check if it isnt Upvoted but it's Downvoted then return confirmation for it's existence
      * @returns {object} object
+     * - check if it's `Upvoted` and if it's `Recipe` then `Downvote` and
+     * `increment` `recipe downvotes`
+     * - check if it's neither `Upvoted` nor `Downvoted` then `downvote` and
+     * `increment` `Recipe downvotes`
+     * - check if it isnt `Upvoted` but it's `Downvoted` then return confirmation for it's existence
      */
   static async unlike (req, res) {
+    const Op = Sequelize.Op
     const { userId, recipeId } = req.params
-    const isUpvoted = await Upvote.find({ where: { userId: userId, recipeId: recipeId } })
+    const isUpvoted = await Upvote.find({ where: { [Op.and]: [{ userId: userId }, { recipeId: recipeId }] } })
     const isRecipe = await Recipe.find({where: {id: recipeId}})
-    const isDownvoted = await Downvote.find({ where: { userId: userId, recipeId: recipeId } })
+    const isDownvoted = await Downvote.find({ where: { [Op.and]: [{ userId: userId }, { recipeId: recipeId }] } })
     if (!isRecipe) return res.status(404).send({success: false, message: 'recipe not found'})
     if (isUpvoted && isRecipe && !isDownvoted) {
       const unLikeRecipe = await isRecipe.decrement('upvotes')
@@ -30,32 +32,48 @@ export default class votes {
       const addDownVote = await Downvote.create({ userId: userId, recipeId: recipeId })
       const increaseDownvoteRecipe = await isRecipe.increment('downvotes')
       if (addDownVote && increaseDownvoteRecipe) return res.status(201).send({success: true, message: 'unliked'})
-    } if (isUpvoted && isRecipe && isDownvoted) {
-      const deleteDownvote = await isUpvoted.destroy()
-      if (deleteDownvote) return res.status(404).send({success: true, message: 'deleted'})
-    } else { return res.status(409).send({success: false, message: 'already downvoted'}) }
+    } else {
+      const isDownvotedDelete = await isDownvoted.destroy()
+      if (isDownvotedDelete) {
+        const addDownVote = await Downvote.create({ userId: userId, recipeId: recipeId })
+        const increaseDownvoteRecipe = await isRecipe.increment('downvotes')
+        if (addDownVote && increaseDownvoteRecipe) return res.status(201).send({ success: true, message: 'unliked' })
+      }
+      // return res.status(409).send({success: false, message: 'already downvoted'})
+    }
   }
   /**
      * @description  Recipe like
      * @param {*} req HTTP - Resquest
      * @param {*} res HTTP - Response
-     * @returns {Promise} promise
+     * @returns object
      */
   static async like (req, res) {
+    const Op = Sequelize.Op
     const {recipeId, userId} = req.params
-    const isUpvoted = await Upvote.find({where: {userId: userId, recipeId: recipeId}})
-    if (isUpvoted) {
-      return res.status(409).send({success: false, message: 'already liked'})
-    }
-    const upvoted = await Upvote.create({recipeId: recipeId, userId: userId})
-    if (upvoted) {
-      const isRecipe = await Recipe.findOne({ where: { id: recipeId } })
-      if (isRecipe) {
-        const recipeLiked = await isRecipe.increment('upvotes')
-        if (recipeLiked) {
-          return res.status(201).send({ success: true, message: 'liked' })
-        }
+    const isUpvoted = await Upvote.find({ where: { [Op.and]: [{ userId: userId }, { recipeId: recipeId }] } })
+    const isRecipe = await Recipe.find({where: {id: recipeId}})
+    const isDownvoted = await Downvote.find({where: {[Op.and]: [{userId: userId}, {recipeId: recipeId}]}})
+    if (!isRecipe) return res.status(404).send({success: false, message: 'recipe not found'})
+    if (isDownvoted && isRecipe && !isUpvoted) {
+      const removeUnLikeRecipe = await isRecipe.decrement('downvotes')
+      const liked = await Upvote.create({userId: userId, recipeId: recipeId})
+      if (liked && removeUnLikeRecipe) {
+        const iLikeRecipe = await isRecipe.increment('upvotes')
+        if (iLikeRecipe) return res.status(201).send({success: true, message: 'liked'})
       }
+    } if (!isDownvoted && isRecipe && !isUpvoted) {
+      const liked = await Upvote.create({userId: userId, recipeId: recipeId})
+      const iLikeRecipe = await isRecipe.increment('upvotes')
+      if (liked && iLikeRecipe) return res.status(201).send({success: true, message: 'liked'})
+    } else {
+      const isUpvotedDelete = await isUpvoted.destroy()
+      if (isUpvotedDelete) return this.like()
+      // return res.status(409).send({success: false, message: 'already liked'})
     }
   }
 }
+// if (isUpvoted && isRecipe && isDownvoted) {
+//   const deleteDownvote = await isUpvoted.destroy()
+//   if (deleteDownvote) return res.status(404).send({ success: true, message: 'deleted' })
+// }
