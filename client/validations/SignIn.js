13060bd5eb2ca2b4dd41f3d098812d7dd73f669c
akolliy1 <adesanmiakoladedotun@gmail.com>
2018-07-13 18:51:53 +0100

    new update

---
+++ client/validations/SignIn.js
@@ -0,0 +1,12 @@
+/**
+ * @function validSignin
+ * @param {*} validateItem
+ * @param {*} eventHandler
+ * @returns {js} validation
+ */
+/* eslint-disable */
+const validSignin = (validateItem, eventHandler) => {
+
+};
+export default validSignin;
+
