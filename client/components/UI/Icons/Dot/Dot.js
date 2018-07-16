138543a8c8ad11a7187fcb76cc4cd63b14b3551a
akolliy1 <adesanmiakoladedotun@gmail.com>
2018-07-16 16:20:33 +0100

    new changes

---
+++ client/components/UI/Icons/Dot/Dot.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import classes from './Dot.css';
+
+/**
+ * @function Dot
+ * @param {*} props
+ * @returns {JSX} jsx
+ * @example clickable dot eg(:)
+ */
+/* eslint-disable */
+const dot = props => (
+  <div className={classes.Dot}>
+    <div />
+    <div />
+    <div />
+  </div>
+);
+
+export default dot;
+
