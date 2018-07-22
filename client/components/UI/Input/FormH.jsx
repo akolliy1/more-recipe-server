60c7d576e71d9a2de4fc95361cb97692504fcf83
akolliy1 <adesanmiakoladedotun@gmail.com>
2018-07-22 22:09:28 +0100

    new update

--- client/components/UI/Input/FormH.jsx
+++ client/components/UI/Input/FormH.jsx
@@ -2,25 +2,34 @@ import React from 'react';
 import Aux from '../../../hoc/Auxs/Auxs';
 import classes from './FormH.css';
 import bgImage from '../../../assets/Images/sitttin-img-1.jpg';
+
 /**
  * @description Header form
+ *
  * @function HeaderForm
+ *
  * @param {*} props
+ *
  * @returns {JSX} jsx
  */
 const HeaderForm = props => (
+  /* eslint-disable */
   <Aux>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.min.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/2.27.5/css/uikit.min.css" />
-    <img src={bgImage} alt="background" className={classes.bgImage} />
+    {/* <img src={bgImage} alt="background" className={classes.bgImage} /> */}
     <div className={['container', classes.Justify, 'mt-4'].join(' ')}>
       <div className={classes.JustifyContentCenter}>
         <div className="card mt-2">
           <div className="field card-body">
             <h2>More-recipe-server</h2>
             <hr />
-            <form action="" method="post">
+            <form>
               {/* eslint-disable */}
+              <div className="error-message">
+                {props.resMsg}
+              </div>
+
               {props.children}
               {/* eslint-enable */}
             </form>
