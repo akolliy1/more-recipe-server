b5f4c7223267cb3b1072d13d0c343650beb72333
akolliy1 <adesanmiakoladedotun@gmail.com>
2018-07-22 22:16:17 +0100

    new update

---
+++ client/components/UI/Spinner/Spinner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import classes from './Spinner.css';
+/**
+ * @function Spinner
+ * @returns {JSX} jsx
+ */
+const Spinner = () => (
+  <div className={classes.Spinner} />
+);
+
+export default Spinner;
